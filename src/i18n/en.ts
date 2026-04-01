1f624e13d7f397bbc2fd6be3ee326833adb8e0c1
MauricioTdM <mauricio1tmelo@yahoo.com.br>
2026-04-01 01:41:08 -0300

    feature: initial project setup with vite and dependencies

---
+++ src/i18n/en.ts
@@ -0,0 +1,7 @@
+export const en = {
+  translation: {
+    "search_placeholder": "Search GitHub username...",
+    "not_found": "There are no users with this name",
+    "repositories": "Repositories",
+  }
+};
---
+++ src/i18n/pt-br.ts
@@ -0,0 +1,7 @@
+export const ptBR = {
+  translation: {
+    "search_placeholder": "Pesquise o usuário do GitHub...",
+    "not_found": "Não há usuários com esse nome",
+    "repositories": "Repositórios",
+  }
+};
